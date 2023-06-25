6d4ef43d5fea7cdb3aebf4eaa741cc06570d976a
Tinsa <tinsa.baby@whitehatjr.com>
2023-06-25 20:02:26 +0530

    adding bsics python also

---
+++ 27-05-2022/python_basics.py
@@ -0,0 +1,18 @@
+print("Hello World")
+# python doesn't reuired types declaration
+a=1
+b=2.1
+a2="fff"
+c=False
+e=None
+name=input("enter u name")
+print(f"Hello ,{name}")
+# condtion in python
+# indentation is required on python
+a=34
+if a<50:
+    print("Good")
+elif a==90:
+    print("none")
+else:
+    print("very good")
--- README.md
+++ README.md
@@ -7,3 +7,9 @@ web programming using python and html,css,js
  git add *
  git commit -m "msg u need".
  git push -u origin main.
+
+ Data structure in python
+ list-sequence of mutable values
+ tuple-sequence of immutable values
+ set- collection of unique values
+ dict-collection of key-value pair
